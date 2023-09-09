6020f7ccb9071f9151bd85343c27cdf38bcf85b2
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-09 14:49:24 -0400

    phase-0-intro-to-js-2-looping-code-along/
    phase-0-intro-to-js-2-looping-code-along/

--- index.js
+++ index.js
@@ -51,19 +51,20 @@ for (let age = 30; age < 40; age++) {
 
 
   ////writeCards() that accepts two arguments: an array of string names, and an event name. Create a for loop with a counter that starts at 0 and increments at the end of each loop. The for loop should stop once it has iterated over the length of the array. As with our previous wrapGifts() function, you will create a custom message for each name inside the loop. Unlike that example, however, instead of simply logging the messages to the console, you will collect them in a new array and return this array at the end of the function. (Refer back to the Array Methods lesson if you need a refresher on how we can add an element to an array.) The overall process should be: create a new, empty array to hold the messages; iterate through the input array and, inside the loop, build out the 'thank you' message for each name using string interpolation, then add that message to the new array you created; after the loop finishes and all of the messages have been added to the new array, return the new array.
-
   
-  function writeCards("Apple,Google,Microsoft") {
-    for (let i = 0; i < names.length;++}
-    messages.push('Wrapped ${names [i'} and added a mouse!');
+  
+  function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Wrapped ${names[i]} and added a mouse!`);
     }
-
-       
-   return messages;
-}
-
-const gifts = ["Apple", "Microsoft", "Google"];
-console.log(writeCards(gifts, "birthday"));
+    return messages;
+  }
+  
+  const gifts = ["Apple", "Microsoft", "Google"];
+  console.log(writeCards(gifts, "birthday"));
+  const gifts = ["Apple", "Microsoft", "Google"];
+  console.log(writeCards(gifts, "birthday"));
 
 
 
