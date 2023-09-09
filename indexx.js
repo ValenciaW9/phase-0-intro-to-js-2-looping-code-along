367d0d40945fbe872e7b235a6f32d1d3fda80355
Valencia Walker <129445679+ValenciaW9@users.noreply.github.com>
2023-09-09 15:18:57 -0400

    phase-0-intro-to-js-2-looping-code-along/
    phase-0-intro-to-js-2-looping-code-along/

--- indexx.js
+++ indexx.js
@@ -1,14 +1,15 @@
- function writeCards(names, event) {
+function writeCards(names, event) {
     let messages = [];
     for (let i = 0; i < names.length; i++) {
       messages.push(`Wrapped ${names[i]} and added a mouse!`);
     }
+    const gifts = ["Apple", "Microsoft", "Google"];
+    console.log(writeCards(gifts, "birthday"));
     return messages;
-    
+  }
+  
   const gifts = ["Apple", "Microsoft", "Google"];
   console.log(writeCards(gifts, "birthday"));
-
-
 
 
 
